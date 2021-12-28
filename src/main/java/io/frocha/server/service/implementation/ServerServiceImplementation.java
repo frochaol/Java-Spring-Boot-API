7a9c36e92d07f316a1758c7a6ef09aaf01aec1fb
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2021-12-28 07:17:51 -0500

    Service implementation create, ping services

--- src/main/java/io/frocha/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/io/frocha/server/service/implementation/ServerServiceImplementation.java
@@ -1,19 +1,43 @@
 package io.frocha.server.service.implementation;
 
+import io.frocha.server.enumeration.Status;
 import io.frocha.server.model.Server;
+import io.frocha.server.repo.ServerRepo;
 import io.frocha.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 
+import static io.frocha.server.enumeration.Status.SERVER_DOWN;
+import static io.frocha.server.enumeration.Status.SERVER_UP;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
+    private final ServerRepo serverRepo;
+
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
@@ -33,6 +57,9 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+    private String setServerImageUrl() {
         return null;
     }
 }
